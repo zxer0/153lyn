d5537a9f0de5461ce9f0a8dcaff9cf9a2a618dbe
Zxer0 <matthew.self@gmail.com>
2016-07-14 06:02:00 -0400

    IG integration, weather integration not working..

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,15 +3,14 @@ $(document).ready(function() {
 	var feed = new Instafeed({
         get: 'tagged',
         tagName: 'rheabeary',
+        limit: '1',
+        resolution: 'low_resolution',
         accessToken: '264011630.bb7baeb.d344f672a17c4ba1a47a75beeaeb8731'
     });
     feed.run();
 
+    $(".weather").weatherFeed({relativeTimeZone:true});
+
 
 
 });
-
-
-/* 
-* https://www.instagram.com/oauth/authorize/?client_id=bb7baebb28fa40d8b21fa32f095f9e5a&redirect_uri=http://www.matthewself.com&response_type=token&scope=public_content
-*/
